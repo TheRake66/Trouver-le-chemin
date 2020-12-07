fdcb71112b2b2e5080d47459600937e8784283bb
TheRake66 <thibault.bustos1234@gmail.com>
2020-12-07 21:19:51 +0100

    Test

---
+++ Trouver le chemin/Case.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trouver_le_chemin
+{
+    class Case
+    {
+        // -------------------------------------------
+        // Static
+        static readonly Color ColorVide = Color.White;
+        static readonly Color ColorCheck = Color.Yellow;
+        static readonly Color ColorCurrent = Color.Orange;
+        static readonly Color ColorValide = Color.Lime;
+        static readonly Color ColorObstacle = Color.Gray;
+        static readonly Color ColorDebut = Color.Green;
+        static readonly Color ColorFin = Color.Red;
+
+        public enum CaseType
+        {
+            Vide,
+            Obstacle,
+            Debut,
+            Fin
+        }
+
+        public static Case[,] LesCases;
+        public static Case LeDebut;
+        public static Case LaFin;
+        // -------------------------------------------
+
+
+
+        // -------------------------------------------
+        // Proptiétés
+        public Label LaGui;
+        public CaseType LeType;
+        public int X;
+        public int Y;
+        public int H;
+        public int G;
+        public int F { get { return this.H + this.G; } }
+        // -------------------------------------------
+
+
+
+        // -------------------------------------------
+        // Constructor
+        Case(Label LaGui, int X, int Y, CaseType LeType)
+        {
+            this.LaGui = LaGui;
+            this.X = X;
+            this.Y = Y;
+            this.LeType = LeType;
+
+            if (LeType == CaseType.Debut) Case.LeDebut = this;
+            else if (LeType == CaseType.Fin) Case.LaFin = this;
+        }
+        // -------------------------------------------
+
+
+
+        // -------------------------------------------
+        // Méthode
+        public static void EffacerCases()
+        {
+            foreach (Case UneCase in Case.LesCases)
+            {
+                UneCase.LeType = CaseType.Vide;
+            }
+        }
+        public static void InitialiserCases(int Largeur, int Hauteur, int Taille)
+        {
+            Case.LesCases = new Case[Hauteur, Largeur];
+
+            for (int i = 0; i < Hauteur; i++)
+            {
+                for (int j = 0; j < Largeur; j++)
+                {
+                    Label UnLabel = new Label();
+                    UnLabel.AutoSize = false;
+                    UnLabel.TextAlign = ContentAlignment.MiddleCenter;
+                    UnLabel.Font = new Font("Consolas", 6F, FontStyle.Regular);
+                    UnLabel.BackColor = Case.ColorVide;
+                    UnLabel.Size = new Size(Taille, Taille);
+                    UnLabel.Location = new Point(Taille * j, Taille * i);
+
+                    // Evite les ref memoire au dernier i et j
+                    int iCopy = i;
+                    int jCopy = j;
+
+                    // Creer une case
+                    Case LaCase = new Case(UnLabel, jCopy, iCopy, CaseType.Vide);
+
+                    UnLabel.MouseClick += new MouseEventHandler((a, b) =>
+                    {
+                        switch (b.Button)
+                        {
+                            // Point debut
+                            case MouseButtons.Left:
+                                if (Case.LeDebut != null) Case.LeDebut.LeType = CaseType.Vide;
+                                LaCase.LeType = CaseType.Debut;
+                                Case.LeDebut = LaCase;
+                                break;
+
+                            // Obstacle
+                            case MouseButtons.Middle:
+                                LaCase.LeType = LaCase.LeType == CaseType.Obstacle ? CaseType.Vide : CaseType.Obstacle;
+                                break;
+
+                            // Point fin
+                            case MouseButtons.Right:
+                                if (Case.LaFin != null) Case.LaFin.LeType = CaseType.Fin;
+                                LaCase.LeType = CaseType.Fin;
+                                Case.LaFin = LaCase;
+                                break;
+                        }
+                    });
+
+                    // Ajoute la case créer aux listes
+                    Case.LesCases[i, j] = LaCase;
+                }
+            }
+        }
+        // -------------------------------------------
+
+
+
+        // -------------------------------------------
+        // Fonctions
+        public int CalculerDistance_Manhattan()
+        {
+            int dx = Math.Abs(Case.LeDebut.X - Case.LaFin.X);
+            int dy = Math.Abs(Case.LeDebut.Y - Case.LaFin.Y);
+            int h = dx + dy;
+            return h;
+        }
+        public int CalculerDistance_Diagonal()
+        {
+            int dx = Math.Abs(Case.LeDebut.X - Case.LaFin.X);
+            int dy = Math.Abs(Case.LeDebut.Y - Case.LaFin.Y);
+            int h = Math.Max(dx, dy);
+            return h;
+        }
+        public double CalculerDistance_Diagonal2()
+        {
+            int dx = Math.Abs(Case.LeDebut.X - Case.LaFin.X);
+            int dy = Math.Abs(Case.LeDebut.Y - Case.LaFin.Y);
+            double h = (dx + dy) + (Math.Sqrt(2) - 2) * Math.Min(dx, dy);
+            return h;
+        }
+        public double CalculerDistance_Euclidean()
+        {
+            int dx = (Case.LaFin.X - Case.LeDebut.X);
+            int dy = (Case.LaFin.Y - Case.LeDebut.Y);
+            double h = Math.Sqrt(dx * dx + dy * dy);
+            return h;
+        }
+        public int CalculerDistance_Hex()
+        {
+            int dx = Case.LeDebut.X - Case.LaFin.X;
+            int dy = Case.LeDebut.Y - Case.LaFin.Y;
+            int dz = dx - dy;
+            int h = Math.Max(Math.Abs(dx), Math.Max(Math.Abs(dy), Math.Abs(dz)));
+            return h;
+        }
+        // -------------------------------------------
+    }
+}
--- Trouver le chemin/Form1.cs
+++ Trouver le chemin/Form1.cs
@@ -12,44 +12,6 @@ namespace Trouver_le_chemin
 {
     public partial class Form1 : Form
     {
-        Label[,] lesCases;
-            int nbCols;
-            int nbLigns;
-            int tailleCase;
-
-        Label debut;
-            int debutX;
-            int debutY;
-        Label fin;
-            int finX;
-            int finY;
-
-
-        int derniereVoisinDistance;
-        int bonX;
-        int bonY;
-        List<int[]> bonVoisinsTrouves = new List<int[]>();
-        List<Label> caseDejaVerifiee = new List<Label>();
-        List<Label> bonChemin = new List<Label>();
-        List<Label> caseBannie = new List<Label>();
-
-
-        int lastVoisinIndex;
-        int lastVoisinX;
-        int lastVoisinY;
-        List<Label> cheminLePlusCourt = new List<Label>();
-
-
-        // Couleur des cases
-        Color colorVide = Color.White;
-        Color colorCheck = Color.Yellow;
-        Color colorCurrent = Color.Orange;
-        Color colorValide = Color.Lime;
-        Color colorObstacle = Color.Gray;
-        Color colorDebut = Color.Green;
-        Color colorFin = Color.Red;
-
-
         public Form1()
         {
             InitializeComponent();
@@ -82,7 +44,7 @@ namespace Trouver_le_chemin
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
             // Si aucun debut ni fin selectionnée
-            if (this.debut == null || this.fin == null)
+            if (Case.LeDebut == null || Case.LaFin == null)
             {
                 MessageBox.Show("Veuillez choisir un point de début et un point de fin. Pour ce faire :" + Environment.NewLine +
                                 "     • Clique gauche : Placer le point de début." + Environment.NewLine +
@@ -91,20 +53,9 @@ namespace Trouver_le_chemin
             }
             else
             {
-                // Reset les cases
-                foreach (Label uneCase in this.lesCases)
-                {
-                    uneCase.Text = "";
-                    changeCaseColor(uneCase, this.colorVide);
-                }
-                this.toolStripStatusLabel1.Text = "";
-                this.caseBannie.Clear();
-                this.bonVoisinsTrouves.Clear();
-                this.bonChemin.Clear();
-                this.caseDejaVerifiee.Clear();
-                this.cheminLePlusCourt.Clear();
                 // Lance une recherche
-                pathFinding(this.debutX, this.debutY);
+                effacerCases();
+                pathFinding();
             }
         }
         private void toolStripLabel4_Click(object sender, EventArgs e)
@@ -112,190 +63,47 @@ namespace Trouver_le_chemin
             this.toolStripLabel4.Checked = !this.toolStripLabel4.Checked;
         }
 
+        private void effacerCases()
+        {
+
+        }
         private void initialiseCases()
         {
             // Defini les tailles avec les textbox
-            this.nbLigns = Convert.ToInt32(this.toolStripTextBox1.Text);
-            this.nbCols = Convert.ToInt32(this.toolStripTextBox2.Text);
-            this.tailleCase = Convert.ToInt32(this.toolStripTextBox3.Text);
+            int Hauteur = Convert.ToInt32(this.toolStripTextBox1.Text);
+            int Largeur = Convert.ToInt32(this.toolStripTextBox2.Text);
+            int Taille = Convert.ToInt32(this.toolStripTextBox3.Text);
 
             // Redimenssione la fenetre
-            while (this.panel1.Width < this.tailleCase * this.nbCols) { this.Width++; }
-            while (this.panel1.Height < this.tailleCase * this.nbLigns) { this.Height++; }
-            while (this.panel1.Width > this.tailleCase * this.nbCols) { this.Width--; }
-            while (this.panel1.Height > this.tailleCase * this.nbLigns) { this.Height--; }
-            // Recentre la fenetre
-            this.Location = new Point(0, 0);
+            this.Width += this.panel1.Width - Largeur * Taille;
+            this.Height += this.panel1.Height - Hauteur * Taille;
 
             // Efface et reset les cases
             this.panel1.Controls.Clear();
-            this.lesCases = new Label[this.nbLigns, this.nbCols];
-
-            for (int i = 0; i < this.nbLigns; i++)
+            Case.InitialiserCases(Largeur, Hauteur, Taille);
+            foreach (Case UneCase in Case.LesCases)
             {
-                for (int j = 0; j < this.nbCols; j++)
-                {
-                    // Creer une case
-                    Label uneCase = new Label();
-                    uneCase.AutoSize = false;
-                    uneCase.TextAlign = ContentAlignment.MiddleCenter;
-                    uneCase.Font = new Font("Consolas", 6F, FontStyle.Regular);
-                    uneCase.BackColor = this.colorVide;
-                    //uneCase.BorderStyle = BorderStyle.FixedSingle;
-                    uneCase.Size = new Size(this.tailleCase, this.tailleCase);
-                    uneCase.Location = new Point(this.tailleCase * j, this.tailleCase * i);
-
-                    // Evite les ref memoire au dernier i et j
-                    int iCopy = i;
-                    int jCopy = j;
-
-                    uneCase.MouseClick += new MouseEventHandler((a, b) =>
-                    {
-                        switch (b.Button)
-                        {
-                            // Point debut
-                            case MouseButtons.Left:
-                                if (this.debut != null) this.debut.BackColor = this.colorVide;
-                                uneCase.BackColor = this.colorDebut;
-                                this.debut = uneCase;
-                                this.debutX = jCopy;
-                                this.debutY = iCopy;
-                                break;
-
-                                // Obstacle
-                            case MouseButtons.Middle:
-                                if (uneCase.BackColor == this.colorObstacle) uneCase.BackColor = this.colorVide;
-                                else uneCase.BackColor = this.colorObstacle;
-                                break;
-
-                            // Point fin
-                            case MouseButtons.Right:
-                                if (this.fin != null) this.fin.BackColor = this.colorVide;
-                                uneCase.BackColor = this.colorFin;
-                                this.fin = uneCase;
-                                this.finX = jCopy;
-                                this.finY = iCopy;
-                                break;
-                        }
-                    });
-
-                    // Ajoute la case créer aux listes
-                    this.lesCases[i, j] = uneCase;
-                    this.panel1.Controls.Add(uneCase);
-                }
+                this.panel1.Controls.Add(UneCase.LaGui);
             }
         }
         private void changeCaseColor(Label uneCase, Color uneColor)
         {
+            /*
             // Change la couleur d'une case en verifiant si pas debut ni fin ni obstacle
-            if (uneCase != this.debut && uneCase != this.fin && uneCase.BackColor != this.colorObstacle) uneCase.BackColor = uneColor;
+            if (uneCase != Case.LeDebut && uneCase != Case.LaFin && uneCase.BackColor != this.colorObstacle)
+            {
+                uneCase.BackColor = uneColor;
+                if (this.toolStripLabel4.Checked) uneCase.Refresh();
+            }*/
         }
 
         private void checkVoisin(int x, int y)
         {
-            // Si la case n'a pas déjà été testée et que c'est pas un obstacle
-            if (!this.caseBannie.Contains(this.lesCases[y, x]) && !this.caseDejaVerifiee.Contains(this.lesCases[y, x]) && this.lesCases[y, x].BackColor != this.colorObstacle)
-            {
-                changeCaseColor(this.lesCases[y, x], this.colorCheck);
-                // Calcul la distance entre deux points sans tenir compte des obstacle
-                int calc = Math.Abs(this.finX - x) + Math.Abs(this.finY - y);
-
-                // Si distance plus courte que la precedente ou qu'aucune precedente
-                if (calc < this.derniereVoisinDistance || this.derniereVoisinDistance == -1)
-                {
-                    this.bonVoisinsTrouves.Clear();
-                    this.derniereVoisinDistance = calc;
-                    this.bonX = x;
-                    this.bonY = y;
-                }
-                // Si la même que precedente on la met dans une liste
-                else if (calc == this.derniereVoisinDistance)
-                {
-                    this.bonVoisinsTrouves.Add(new int[] { x, y });
-                }
-            }
         }
-        private void pathFinding(int x, int y)
+        private void pathFinding()
         {
-            // Chemin trouvé
-            if (x == this.finX && y == this.finY)
-            {
-                pathFounded(this.debutX, this.debutY);
-            }
-            else
-            {
-                // Reset
-                this.derniereVoisinDistance = -1;
-
-                // Couleur en cours orange
-                changeCaseColor(this.lesCases[y, x], this.colorCurrent);
-
-                // On vérifie toute les cases voisines
-                if (y - 1 >= 0 && x + 1 < this.lesCases.GetLength(1)) checkVoisin(x + 1, y - 1);                          // Haut droit
-                if (x - 1 >= 0 && y - 1 >= 0) checkVoisin(x - 1, y - 1);                                                  // Haut gauche
-                if (x - 1 >= 0 && y + 1 < this.lesCases.GetLength(0)) checkVoisin(x - 1, y + 1);                          // Bas gauche
-                if (x + 1 < this.lesCases.GetLength(1) && y + 1 < this.lesCases.GetLength(0)) checkVoisin(x + 1, y + 1);  // Bas droit
-                if (y - 1 >= 0) checkVoisin(x, y - 1);                                                                    // Haut
-                if (x - 1 >= 0) checkVoisin(x - 1, y);                                                                    // Gauche
-                if (x + 1 < this.lesCases.GetLength(1)) checkVoisin(x + 1, y);                                            // Droite
-                if (y + 1 < this.lesCases.GetLength(0)) checkVoisin(x, y + 1);                                            // Bas
-
-                // Si plusieurs voisins de même longueurs trouvés on en tire un au pif
-                if (this.bonVoisinsTrouves.Count > 1)
-                {
-                    int rnd = (new Random()).Next(0, this.bonVoisinsTrouves.Count);
-                    this.bonX = this.bonVoisinsTrouves[rnd][0];
-                    this.bonY = this.bonVoisinsTrouves[rnd][1];
-                }
-
-                // Bloqué
-                if (this.derniereVoisinDistance == -1)
-                {
-                    // Reviens en arrière
-                    this.bonChemin.Clear();
-                    this.caseDejaVerifiee.Clear();
-                    this.caseBannie.Add(this.lesCases[y, x]); // Dit que la case vient d'être traitée
-                    pathFinding(this.debutX, this.debutY);
-                }
-                else
-                {
-                    // Voisin le plus proche
-                    this.bonChemin.Add(this.lesCases[this.bonY, this.bonX]);
-
-                    // Refresh et recursive
-                    this.caseDejaVerifiee.Add(this.lesCases[y, x]); // Dit que la case vient d'être traitée
-                    if (this.toolStripLabel4.Checked) this.Refresh();
-                    pathFinding(this.bonX, this.bonY);
-                }
-            }
-        }
-
-        private void checkVoisinFounded(int x, int y)
-        {
-            // Trouve le chemin le plus court partie tous les chemin trouvés
-            this.lesCases[y, x].Text = this.bonChemin.IndexOf(this.lesCases[y, x]).ToString();
-            if (this.bonChemin.Contains(this.lesCases[y, x]) && (this.bonChemin.IndexOf(this.lesCases[y, x]) > this.lastVoisinIndex || this.lastVoisinIndex == -1))
-            {
-                this.lastVoisinIndex = this.bonChemin.IndexOf(this.lesCases[y, x]);
-                this.lastVoisinX = x;
-                this.lastVoisinY = y;
-            }
-        }
-        private void pathFounded(int x, int y)
-        {
-            // Chemin trouvé
-            if (x == this.finX && y == this.finY)
-            {
-                foreach (Label uneCase in this.cheminLePlusCourt)
-                {
-                    changeCaseColor(uneCase, this.colorValide);
-                }
-
-                this.toolStripStatusLabel1.Text = "Chemin trouvé avec une taille de : " + this.cheminLePlusCourt.Count;
-            }
-            else
-            {
-                this.lastVoisinIndex = -1;
+            /*
+             * 
 
                 // On vérifie toute les cases voisines
                 if (y - 1 >= 0 && x + 1 < this.lesCases.GetLength(1)) checkVoisinFounded(x + 1, y - 1);                          // Haut droit
@@ -305,15 +113,7 @@ namespace Trouver_le_chemin
                 if (y - 1 >= 0) checkVoisinFounded(x, y - 1);                                                                    // Haut
                 if (x - 1 >= 0) checkVoisinFounded(x - 1, y);                                                                    // Gauche
                 if (x + 1 < this.lesCases.GetLength(1)) checkVoisinFounded(x + 1, y);                                            // Droite
-                if (y + 1 < this.lesCases.GetLength(0)) checkVoisinFounded(x, y + 1);
-
-                // Prend le voisin avec le plus de poids dans la liste des chemins trouvé
-                if (this.lastVoisinIndex != -1) this.cheminLePlusCourt.Add(this.lesCases[this.lastVoisinY, this.lastVoisinX]);
-
-                // Refresh et recursive
-                if (this.toolStripLabel4.Checked) this.Refresh();
-                pathFounded(this.lastVoisinX, this.lastVoisinY);
-            }
+                if (y + 1 < this.lesCases.GetLength(0)) checkVoisinFounded(x, y + 1);*/
 
         }
     }
